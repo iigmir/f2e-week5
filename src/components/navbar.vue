28587de21e7b94709db71cbdeb41f3476aa280c8
iigmir <roc120j@gmail.com>
2018-07-02 23:09:42 +0800

    firstly nav

---
+++ src/components/navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav>
+        <router-link to="/"> Comicomic </router-link>
+    </nav>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default class navbar extends Vue {
+};
+</script>
+
+
+<style lang="scss">
+</style>
